0c856177afe16f421aa2e264390cfc9b84d13008
arifnayan2861 <am.nayan2861@gmail.com>
2024-06-03 21:04:30 +0600

    donation request data added

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       res.send(result);
     });
 
+    //get donation request data of a user
+    app.get("/donation-requests/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await donationsCollection
+        .find({ requesterEmail: email })
+        .toArray();
+      res.send(result);
+    });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
